ffadb11bad124a8c7abcca77d3f49e0aefb94c9f
dhwpdnr <ink1237@gmail.com>
2022-11-17 14:47:25 +0900

    login signup html / signup post

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Linux'
+@app.route('/login')
+def login():  # put application's code here
+    if (request.method == 'GET'):
+        return render_template('login.html')
+
+    elif (request.method == 'POST'):
+        # 아이디 비밀번호 확인하는 로직
+        return redirect(url_for())
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if (request.method == 'GET'):
+        return render_template('signup.html')
+
+    elif (request.method == 'POST'):
+        value = request.form
+        user_id = request.form["user_id"]
+        user_pw = request.form["user_pw"]
+        user_name = request.form["user_name"]
+
+        #이부분에 DB에 저장하는 로직 들어감
+
+        print(value)
+        print(user_id)
+        print(user_pw)
+        print(user_name)
+        return redirect(url_for('login'))
+
+
+# @app.route('/api/signup', methods=['POST'])
+# def api_signup():
+
 
 
 if __name__ == '__main__':
